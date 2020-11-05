378c8f3b15c98b9b7c0ae12a67576d0823d632db
Strahinja <skansi97@gmail.com>
2020-11-05 22:12:56 +0100

    Fixing mistake in Navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -5,23 +5,53 @@
         <router-link to="/" class="navbar-brand">Main Page</router-link>
       </div>
       <div id="otherpages">
-        <router-link to="/login" class="navbar-brand login" v-if="!isUserAuthenticated"
+        <router-link
+          to="/login"
+          class="navbar-brand login"
+          v-if="!isUserAuthenticated"
           >Login</router-link
         >
-        <router-link to="/register" class="navbar-brand register" v-if="!isUserAuthenticated">Register</router-link>
-        <button v-if="isUserAuthenticated" @click="onLogout" type="button" class="btn btn-alert btn-sm">Log out</button>
+        <router-link
+          to="/register"
+          class="navbar-brand register"
+          v-if="!isUserAuthenticated"
+          >Register</router-link
+        >
+        <button
+          v-if="isUserAuthenticated"
+          @click="onLogout"
+          type="button"
+          class="btn btn-alert btn-sm"
+        >
+          Log out
+        </button>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import { mapGetters, mapActions } from "vuex";
 export default {
   name: "NavBar",
 
-  components: {
-      
-  }
+  components: {},
+  computed: {
+    ...mapGetters({
+      isUserAuthenticated: "auth/isUserAuthenticated",
+    }),
+  },
+  methods: {
+    ...mapActions({
+      logout: "auth/logout",
+    }),
+    onLogout() {
+      this.logout();
+      this.$router.push({
+        name: "login",
+      });
+    },
+  },
 };
 </script>
 
