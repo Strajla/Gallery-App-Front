c08060b66a609d0181f7c5da2d3e557ea3f7f405
Strahinja <skansi97@gmail.com>
2020-11-06 14:34:34 +0100

    Adding stuff to GalleryC comp

--- src/components/GalleryCard.vue
+++ src/components/GalleryCard.vue
@@ -1,5 +1,14 @@
 <template>
-    
+<div>
+    <div class="card" style="width: 300px; height: 300px; margin-top: 30px ">
+  <img class="card-img-top" src="..." alt="Card image cap">
+  <h4 class="title">{{gallery.name}} </h4>
+    <ol class="list-group list-group-flushh">
+      <li class="list-group-item">{{gallery.user.first_name}}</li>
+      <li class="list-group-item">{{gallery.user.last_name}}</li>
+    </ol>  
+</div>
+</div>
 </template>
 
 <script>
