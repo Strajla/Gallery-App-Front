a6dc584800e17c512cfafe470f5837cec408f6a5
Strahinja <skansi97@gmail.com>
2020-11-06 15:57:25 +0100

    Finishing component and fixing small mistake

--- src/components/Author.vue
+++ src/components/Author.vue
@@ -1,27 +1,34 @@
 <template>
-
+  <div>
+    <h2>Creators</h2>
     <div>
-
-        <h2>Creators</h2>
-        <div>
-            <h3>First Name</h3>
-        <div>
-            <h4>Author name</h4>
-            <p>Description</p>
-        </div>
-
+      <h3>{{ author.first_name }}</h3>
+      <div v-for="author in author.galleries" :key="author.id">
+        <h4>Author name</h4>
+        <p>Description</p>
+      </div>
     </div>
-
-    </div>
-    
+  </div>
 </template>
 
 <script>
+import { authors } from "../services/Author";
+import { mapActions } from "vuex";
 export default {
-    
-}
+  data() {
+    return {
+      author: [],
+    };
+  },
+
+  async created() {
+    this.author = (await authors.getOne(this.$route.params.id)).data;
+  },
+
+  methods: {
+    ...mapActions(["getOneAuthor"]),
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
--- src/components/GalleryCard.vue
+++ src/components/GalleryCard.vue
@@ -4,7 +4,7 @@
   <img class="card-img-top" :src="gallery.images[0].source" alt="Card image cap">
   <h4 class="card-title"><router-link :to="{ name: 'gallery', params: {id: gallery.id }}" >{{gallery.name}}</router-link></h4>
     <ol style="list-style-type:upper-roman" class="list-group list-group-flushh">
-      <li class="list-group-item">{{gallery.user.first_name}}</li>
+      <li class="list-group-item"><router-link :to="{ name: 'authors', params: {id: gallery.user.id }}">{{gallery.user.first_name}}</router-link></li>
       <li class="list-group-item">{{gallery.user.last_name}}</li>
       <li class="list-group-item">{{gallery.created_at}}</li>
     </ol>  
@@ -17,6 +17,7 @@ export default {
     name: 'GalleryCard',
   props: {
         gallery :Object,
+        user: Object
     },
 }
 
