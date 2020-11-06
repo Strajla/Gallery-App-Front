f7266a7b2a0c4f3a5ce84a855eba8f7d0b2dd8c8
Strahinja <skansi97@gmail.com>
2020-11-06 15:22:34 +0100

    Fixing mistakes in store and Gallery comp

--- src/components/Gallery.vue
+++ src/components/Gallery.vue
@@ -15,7 +15,7 @@ import {mapActions} from 'vuex'
 export default {
      data() {
         return {
-            galleries:[],
+            gallery:[],
         }
     },
     async created() {
--- src/vuex/store.js
+++ src/vuex/store.js
@@ -9,7 +9,7 @@ Vue.use(Vuex)
 export const store = new Vuex.Store({
     state: {
         galleries: [],
-        images: {}
+        images: [],
     },
 
     getters: {
