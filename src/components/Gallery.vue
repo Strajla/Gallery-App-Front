4858abd58dfbed535a9ee6a663169873385155f3
Strahinja <skansi97@gmail.com>
2020-11-06 23:40:02 +0100

    Fixing mistakes

--- src/components/Gallery.vue
+++ src/components/Gallery.vue
@@ -2,7 +2,7 @@
   <div>
     <h2>Gallery</h2>
     <h2>{{ gallery.name }}</h2>
-    <h3>{{gallery.user.id}}</h3>
+    <h3>{{ gallery.user.id }}</h3>
     <h4>
       <router-link :to="{ name: 'authors', params: { id: gallery.user.id } }"
         >{{ gallery.user.first_name }} {{ gallery.user.last_name }}</router-link
@@ -11,10 +11,16 @@
     <p>{{ gallery.created_at }}</p>
     <p>{{ gallery.description }}</p>
 
-  <div class="card-body" >
-
-                <button type="button" @click="deleteGallery(gallery.id)"  v-if="isUserAuthenticated && gallery.user.id == loggedUser.id" class="btn btn-danger">Delete Gallery</button>
-          </div>
+    <div class="card-body">
+      <button
+        type="button"
+        @click="deleteGallery(gallery.id)"
+        v-if="isUserAuthenticated && gallery.user.id == loggedUser.id"
+        class="btn btn-danger"
+      >
+        Delete Gallery
+      </button>
+    </div>
 
     <div>
       <b-carousel
@@ -37,23 +43,37 @@
     </div>
 
     <h3>Comments:</h3>
-    <div v-for="comment in gallery.comments" :key="comment.id" :comment="comment">
-            <p>{{comment.user_id}} <span>{{comment.text}}</span></p> 
-           <button v-if="isUserAuthenticated && comment.user_id == loggedUser.id">Delete</button>
+    <div
+      v-for="comment in gallery.comments"
+      :key="comment.id"
+      :comment="comment"
+    >
+      <p>
+        {{ comment.user_id }} <span>{{ comment.text }}</span>
+      </p>
+      <button
+        v-if="isUserAuthenticated && comment.user_id == loggedUser.id"
+        @click="deleteComment(comment.id)"
+      >
+        Delete
+      </button>
     </div>
-     <div class>
+    <div class>
       <div class="row">
-
-              <form @submit.prevent="onSubmit" class="form-inline" v-if="isUserAuthenticated">
-                <textarea
-                  v-model="form.text"
-                  placeholder="Write your comment here!"
-                  class="pb-cmnt-textarea"
-                ></textarea> 
-                <button class="btn btn-primary pull-right" type="submit">Add Comment</button>
-              </form>
-
-
+        <form
+          @submit.prevent="onSubmit"
+          class="form-inline"
+          v-if="isUserAuthenticated"
+        >
+          <textarea
+            v-model="form.text"
+            placeholder="Write your comment here!"
+            class="pb-cmnt-textarea"
+          ></textarea>
+          <button class="btn btn-primary pull-right" type="submit">
+            Add Comment
+          </button>
+        </form>
       </div>
     </div>
   </div>
@@ -66,53 +86,50 @@ export default {
   data() {
     return {
       gallery: [],
-      form: { 
-                author: "", 
-                text: "", 
-                user_id: "", 
-                gallery_id: "" 
-                }
-    }
+      form: {
+        author: "",
+        text: "",
+        user_id: "",
+        gallery_id: "",
+      },
+    };
   },
 
-    props: {
-        comments: Object
-    },
+  props: {
+    comments: Object,
+  },
 
   async created() {
-     this.gallery = (await galleries.getOne(this.$route.params.id)).data,
-        this.getLoggedUser();
+    (this.gallery = (await galleries.getOne(this.$route.params.id)).data),
+      this.getLoggedUser();
   },
   methods: {
     ...mapActions({
-            getOne: 'getOne',
-            addComment: 'addComment',
-            getLoggedUser: 'auth/getLoggedUser',
-            deleteGallery: 'deleteGallery'
-        }),
-        onSubmit() {
-    
-            this.form.author = this.user.first_name + " " + this.user.last_name;
-            this.form.author_id = this.user.id;
-            this.form.gallery_id = this.gallery.id;
-            this.addComment(this.form);
-        }
+      getOne: "getOne",
+      addComment: "addComment",
+      getLoggedUser: "auth/getLoggedUser",
+      deleteGallery: "deleteGallery",
+      deleteComment: "deleteComment",
+    }),
+    onSubmit() {
+      this.form.author = this.user.first_name + " " + this.user.last_name;
+      this.form.author_id = this.user.id;
+      this.form.gallery_id = this.gallery.id;
+      this.addComment(this.form);
+    },
   },
   computed: {
     ...mapGetters({
       authors: "authors",
       isUserAuthenticated: "auth/isUserAuthenticated",
-      loggedUser: "auth/loggedUser"
-    })
+      loggedUser: "auth/loggedUser",
+    }),
   },
 };
 </script>
 
-
 <style>
-
 .row {
   margin-left: 650px;
 }
-
 </style>
