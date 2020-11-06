cdb43e5537dde4b6f34990af0004adc859e558b2
Strahinja <skansi97@gmail.com>
2020-11-06 15:08:07 +0100

    Fixing components and adding image service

--- src/components/Gallery.vue
+++ src/components/Gallery.vue
@@ -1,11 +1,31 @@
 <template>
-    
+     <div>
+        <h2>Gallery</h2>
+        <h2>{{gallery.name}}</h2>
+        <div v-for="image in gallery.images" :key="image.id">
+            <img class="card-img-top" :src="image.source" alt="Card image cap">
+        </div>
+    </div>
 
 </template>
 
 <script>
+import {galleries} from '../services/Galleries'
+import {mapActions} from 'vuex'
 export default {
-    
+     data() {
+        return {
+            gallery:[],
+        }
+    },
+    async created() {
+        this.gallery = (await galleries.getOne(this.$route.params.id)).data
+    },
+    methods: {
+        ...mapActions([
+            'getOne'
+        ])
+}
 }
 </script>
 
--- src/components/GalleryCard.vue
+++ src/components/GalleryCard.vue
@@ -2,10 +2,11 @@
 <div>
     <div class="card" style="width: 300px; height: 300px; margin-top: 30px ">
   <img class="card-img-top" :src="gallery.images[0].source" alt="Card image cap">
-  <h4 class="title">{{gallery.name}} </h4>
+  <h4 class="card-title"><router-link :to="{ name: 'gallery', params: {id: gallery.id }}" >{{gallery.name}}</router-link></h4>
     <ol style="list-style-type:upper-roman" class="list-group list-group-flushh">
       <li class="list-group-item">{{gallery.user.first_name}}</li>
       <li class="list-group-item">{{gallery.user.last_name}}</li>
+      <li class="list-group-item">{{gallery.created_at}}</li>
     </ol>  
 </div>
 </div>
